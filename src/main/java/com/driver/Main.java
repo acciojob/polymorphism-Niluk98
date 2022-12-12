21a4f1fdf57f0e4fd3eb8ec080e7c3fed2cf9459
niladri <niladriparamanik@gmail.com>
2022-12-12 13:33:42 +0530

    new commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+          public int product(int x,int y){
+              return x*y;
+          }
+          public  int product(int x,int y,int z){
+              return x*y*z;
+          }
+          public double product(double x, double y){
+                return x*y;
+          }
+    }
 
+    public static void main(String[] args) {
+        Product p=new Product();
+        int x=0;
+        int y=0;
+        int z=0;
+        double a=0;
+        double b=0;
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
+
+    }
 }
